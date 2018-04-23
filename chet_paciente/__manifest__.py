94ae60e117be3793e6ba0cc5945b645932e86497
Humberto Arocha <hbto@vauxoo.com>
2018-04-23 18:06:53 -0400

    [IMP] Adding scaffolding module for patient management

---
+++ chet_paciente/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Registro y Actualización de Pacientes',
+    'version' : '1.0',
+    'summary': 'Permite realizar el registro, búsqueda y actualización de los datos de pacientes',
+    'sequence': 30,
+    'description': """
+Este módulo agrega datos al modelo res.partner para que se pueda utilizar para
+el registro de pacientes. Siendo res.partner un módulo genérico se extiende
+para reutilizarlo con otros recursos.
+    """,
+    'category': 'Medical',
+    'website': 'https://www.vauxoo.com',
+    'images' : [],
+    'depends' : ['base'],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
